76816c25b9110c8f6cf0c31a157c51fdd5273d58
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-03-04 20:28:40 +0100

    created superuser, changes in mainapp models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from.models import Translation
+from.models import Translation,Flashcard,Deck
 
 # Register your models here.
 admin.site.register(Translation)
+admin.site.register(Flashcard)
+admin.site.register(Deck)
--- mainapp/models.py
+++ mainapp/models.py
@@ -4,12 +4,20 @@ class Translation(models.Model):
     input_text = models.CharField(max_length=500)
     output_text = models.CharField(max_length=500, null=True, blank=True)
     is_flashcard = models.BooleanField()
-# Create your models here.
+
+    def __str__(self):
+        return self.input_text
 
 class Flashcard(models.Model):
     front = models.CharField(max_length=500)
     back = models.CharField(max_length=500)
 
+    def __str__(self):
+        return self.front
+
 class Deck(models.Model):
     name = models.CharField(max_length=140)
     flashcards = models.ManyToManyField(Flashcard, blank=True, related_name="decks")
+
+    def __str__(self):
+        return self.name
