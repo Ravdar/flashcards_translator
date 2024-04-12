4305f0fe85c5cd9e6f20de829050f7626ca7fd4e
Ravdar <97836782+Ravdar@users.noreply.github.com>
2024-04-12 23:05:53 +0200

    Minor changes in frontend, started working on deck view page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from . import views
 
 app_name="mainapp"
-urlpatterns = [path("", views.landing_page, name="landing_page"),path("translator/", views.translator, name="translator"), path("review/", views.review, name="review"), path("users/<str:user_username>/", views.user_profile, name="user_profile"),path("decks/", views.decks, name="decks"),
+urlpatterns = [path("", views.landing_page, name="landing_page"),path("translator/", views.translator, name="translator"), path("review/", views.review, name="review"), path("users/<str:user_username>/", views.user_profile, name="user_profile"),path("decks/", views.decks, name="decks"), path("<str:deck_name>/", views.deck, name="deck"),
 path("select2/", include("django_select2.urls")),
                ]
