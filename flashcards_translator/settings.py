7f1c84533c2e2d43b672719177fd0e894c216bc1
ravdar <8squeex8@gmail.com>
2024-05-26 12:36:17 +0200

    Railway deployment part two

--- flashcards_translator/settings.py
+++ flashcards_translator/settings.py
@@ -82,10 +82,14 @@ WSGI_APPLICATION = "flashcards_translator.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+DATABASES = DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('PGDATABASE'),
+        'USER': os.getenv('PGUSER'),
+        'PASSWORD': os.getenv('PGPASSWORD'),
+        'HOST': os.getenv('PGHOST'),
+        'PORT': os.getenv('PGPORT'),
     }
 }
 
